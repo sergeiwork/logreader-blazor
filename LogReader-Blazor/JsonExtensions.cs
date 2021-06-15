512de9c31e18fbf1a7a4ab9399ad364acbed833d
Sergei <sergeiwork@yahoo.com>
2021-06-15 16:55:43 +0300

    bump

---
+++ LogReader-Blazor/JsonExtensions.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using System.Text.Json;
+
+namespace LogReader_Blazor
+{
+    public static partial class JsonExtensions
+    {
+        public static JsonElement? Get(this JsonElement element, string name) =>
+            element.ValueKind != JsonValueKind.Null && element.ValueKind != JsonValueKind.Undefined && element.TryGetProperty(name, out var value)
+                ? value : null;
+
+        public static JsonElement? Get(this JsonElement element, int index)
+        {
+            if (element.ValueKind == JsonValueKind.Null || element.ValueKind == JsonValueKind.Undefined)
+                return null;
+            var value = element.EnumerateArray().ElementAtOrDefault(index);
+            return value.ValueKind != JsonValueKind.Undefined ? value : null;
+        }
+    }
+}
